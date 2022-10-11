1fbf6c5afa5d1169fa286fe1445a31c2014bf9de
Anna <a.chetkina.a@yandex.ru>
2022-10-11 20:34:21 +0300

    add code

---
+++ code/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintShortArray(string[] array)
+{
+     Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] -> ");
+
+    array = array.Where(item => item.Length <= 3).ToArray();
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+string[] array1 = {"hello", "2", "world", ":)"}; 
+string[] array2 = {"1234", "1567", "-2", "computer science"}; 
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+
+PrintShortArray(array1); //  [2, :)]
+PrintShortArray(array2); // [-2]
+PrintShortArray(array3);  // []
